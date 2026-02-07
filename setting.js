46792a8d51c2a4e3a93f3897dd68fbecf1814e2f
jerry-md254 <jerryking900000@gmail.com>
2026-02-07 18:16:46 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩𝐉𝐄𝐑𝐑𝐘 𝐌𝐃𓆪',
+  author: '‎',
+  botName: "𓆩𝐉𝐄𝐑𝐑𝐘 𝐌𝐃𓆪",
+  botOwner: 'SILVER', // Your name
+  ownerNumber: '359876789681', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/jerry-md254/-",
+};
+
+module.exports = settings;
